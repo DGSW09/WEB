e1af7b2b5ac4fd120488c80b9b967fb570a573f0
jeonminchan <chan2bo2@dgsw.hs.kr>
2023-09-05 20:37:18 +0900

    ReCoding: MainPage

--- project09/src/App.js
+++ project09/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import Login from "./components/LoginPage/Login";
 function App() {
   return (
     <div className="App">
-      <a />
+      <Login />
     </div>
   );
 }
--- project09/src/components/MainPage/MainPage.jsx
+++ project09/src/components/MainPage/MainPage.jsx
@@ -1,7 +1,49 @@
+import MainLogo from "../logo.svg";
+import profile from "../profile.svg";
+import Menu from "../menu.svg";
+import Prodct from "../product.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import "./MainPage.css";
+import { useState } from "react";
+
 const MainPage = () => {
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  };
   return (
     <>
-      <div></div>
+      <div className="topNav">
+        <img src={MainLogo} alt="09's MainLogo" className="logo" />
+        <div className="inputWrap">
+          <input
+            type="text"
+            className="searchInput"
+            onChange={handleSearch}
+            value={search}
+          />
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="searchIcon" />
+        </div>
+        <div className="navMenu">
+          <img src={profile} className="profile" alt="" />
+          <img src={Menu} className="menubar" alt="" />
+        </div>
+      </div>
+      <div className="product">
+        <div className="recentProduct">
+          <div className="product">최근에 본 상품</div> <br />
+          <div className="previewProduct">
+            <img src={Prodct} alt="" />
+            <div className="productInfo">
+              <div className="productName">존나존나 많은 앵그리버드</div> <br />
+              <div className="proudctPrice">가격 측정 불가요 1/10</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
