f86190906676182e0f7488dc86d6640d52805933
jeonminchan <chan2bo2@dgsw.hs.kr>
2023-09-06 09:03:14 +0900

    Update: Main.jsx

--- project09/src/components/Main/Main.jsx
+++ project09/src/components/Main/Main.jsx
@@ -1,5 +1,5 @@
 import logo from "../../img/logo.png";
-import "./productInfo.css";
+import "./Main.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
