dcd6ea551a5ace44dc1ef70288390a14e4fecf6f
jeonminchan <chan2bo2@dgsw.hs.kr>
2023-09-08 00:49:21 +0900

    Update: Product.jsx, Product.css

--- project09/src/components/product/Product.jsx
+++ project09/src/components/product/Product.jsx
@@ -1,7 +1,23 @@
 import "./Product.css";
 import Back from "../../img/back.png";
 import productPicture from "../../img/jetti.svg";
+
+let toggle;
+
 const Product = () => {
+  const JoinComplete = () => {
+    const BankAccount = document.getElementById("Join");
+    if (!toggle) {
+      BankAccount.innerText = "참여하기";
+      console.log("clicked 참여하기");
+      toggle = true;
+    } else {
+      BankAccount.innerText = "7777023773096 카카오뱅크";
+      console.log("clicked 카뱅");
+      toggle = false;
+    }
+  };
+
   return (
     <div className="ProductInfoWrap">
       <img src={Back} className="back" />
@@ -11,6 +27,15 @@ const Product = () => {
       </div>
       <div className="productInfo">
         <div className="productTitle">제티 2박스</div>
+        <div className="productPrice">₩ 9,920</div>
+        <div className="nPrice">1/N : ₩4,960</div>
+        <div className="productLink">
+          <a href="#">https://wowjety?-many.com/angrybird/</a>
+        </div>
+        <div className="nMember">1/10</div>
+        <button type="submit" id="Join" onClick={JoinComplete}>
+          참여하기
+        </button>
       </div>
     </div>
   );
