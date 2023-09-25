e104113635d9cf775bc0e70666023a348faaad82
jeonminchan <chan2bo2@dgsw.hs.kr>
2023-09-25 14:43:58 +0900

    Upadte : ProductPage

--- project09/src/components/product/Product.jsx
+++ project09/src/components/product/Product.jsx
@@ -1,9 +1,7 @@
 import "../../styles/Product.css";
 import Back from "../../img/back.png";
-import productPicture from "../../img/jetti.svg";
-
+import ProductImage from "../../img/jetti.svg";
 let toggle;
-
 const Product = () => {
   const JoinComplete = () => {
     const BankAccount = document.getElementById("Join");
@@ -17,25 +15,32 @@ const Product = () => {
   };
 
   return (
-    <div className="ProductInfoWrap">
-      <img src={Back} className="back" alt="" />
-      <div className="WrapTitle">상세페이지</div>
-      <div className="productPicture">
-        <img src={productPicture} alt="" />
+    <div className="ProdcutWrap">
+      <div className="PageInfo">
+        <img src={Back} className="back" />
+        <div className="PageTitle">상세페이지</div>
       </div>
-      <div className="productInfo">
-        <div className="productTitle">제티 2박스</div>
-        <div className="ProductContent">존나존나 많은 제티 2박스요</div>
-        <div className="productPrice">₩ 9,920</div>
-        <div className="nPrice">1/N : ₩4,960</div>
-
-        <div className="productLink">
-          <a href="#">https://wowjety?-many.com/angrybird/</a>
+      <div className="ProductInfo">
+        <img src={ProductImage} className="productImage" />
+        <div className="ProductInfo2">
+          <div className="ProductTitle">제티 2박스</div>
+          <div>
+            <a className="ProductLink" href="#">
+              https://wowjety?-many.com/angrybird/
+            </a>
+          </div>
+          <div className="ProductPrice">₩ 9,920</div>
+          <div className="nProductPrice">1/N : ₩ 4,960</div>
+          <div className="ProductContent">
+            아침에 먹는 우유 같이 제티 타서 먹을 사람....................
+          </div>
+          <div className="member">1/10</div>
+          <div className="ProductJoin">
+            <button id="Join" className="join" onClick={JoinComplete}>
+              공구 참가
+            </button>
+          </div>
         </div>
-        <div className="nMember">1/10</div>
-        <button type="submit" id="Join" onClick={JoinComplete}>
-          참여하기
-        </button>
       </div>
     </div>
   );
