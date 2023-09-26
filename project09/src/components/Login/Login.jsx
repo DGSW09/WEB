4bbea40f741a12d49236dd1530a156eb2f0112c8
jeonminchan <chan2bo2@dgsw.hs.kr>
2023-09-27 00:15:47 +0900

    Update: LoginPage

--- project09/src/components/Login/Login.jsx
+++ project09/src/components/Login/Login.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-// import logo from "../Logo/logo.png";
 import logo from "../../img/logo.png";
 import "../../styles/Login.css";
 
@@ -9,42 +8,42 @@ const SignUp = () => {
   const [Class, setClass] = useState("");
 
   return (
-    <div className="width">
+    <div className="LoginWrap">
       <div className="logo">
         <img src={logo} alt="09's logo" />
-        {/* gonggu */}
       </div>
-      <form method="get">
-        <div className="InputName">
-          <input
-            type="text"
-            placeholder="Your Name"
-            onChange={(e) => setName(e.target.value)}
-            value={Name}
-            maxLength="4"
-          />
-        </div>
-        <div className="InputGrade">
-          <input
-            type="number"
-            placeholder="Your Grade"
-            onChange={(e) => setGrade(e.target.value)}
-            value={Grade}
-            min="1"
-          />
-        </div>
-        <div className="InputClass">
-          <input
-            type="number"
-            placeholder="Your Class"
-            onChange={(e) => setClass(e.target.value)}
-            value={Class}
-            max="99"
-            min="1"
-          />
-        </div>
-        <div className="start">
-          <button type="submit" onClick={() => alert("회원가입")}>
+      <form method="get" className="InputWrap">
+        <input
+          type="text"
+          placeholder="이름을 입력하세요."
+          onChange={(e) => setName(e.target.value)}
+          value={Name}
+          maxLength="4"
+          className="InputName"
+        />
+        <input
+          type="number"
+          placeholder="학년을 입력하세요"
+          onChange={(e) => setGrade(e.target.value)}
+          value={Grade}
+          min="1"
+          className="InputGrade"
+        />
+        <input
+          type="number"
+          placeholder="학반을 입력하세요"
+          onChange={(e) => setClass(e.target.value)}
+          value={Class}
+          max="99"
+          min="1"
+          className="InputClass"
+        />
+        <div>
+          <button
+            type="submit"
+            onClick={() => alert("회원가입")}
+            className="start"
+          >
             시작하기!
           </button>
         </div>
