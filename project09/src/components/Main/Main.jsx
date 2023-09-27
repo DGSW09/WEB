b12c2ec56698d6bd6aadcf303fec12095b85ae8b
jeonminchan <chan2bo2@dgsw.hs.kr>
2023-09-27 11:45:58 +0900

    Update: MainPage

--- project09/src/components/Main/Main.jsx
+++ project09/src/components/Main/Main.jsx
@@ -1,117 +1,13 @@
-import logo from "../../img/logo.png";
-import search from "../../img/search.png";
 import "../../styles/Main.css";
-import profile from "../../img/profile.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwsomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../img/logo.png";
+import profile from "../../img/profile.png";
+
 const Main = () => {
   return (
-    <div className="productInfoWrap">
-      <div className="TopNav">
-        <img src={logo} alt="" className="logo" />
-        <input type="text" className="input" />
-        <img src={search} className="searchIcon" alt="" />
-        <div className="userMenu">
-          <img src={profile} className="profile" alt="" />
-          <FontAwesomeIcon
-            icon={faPenToSquare}
-            className="edit"
-            color="#000000"
-          />
-        </div>
-      </div>
-      <h6 className="recentProductTitle">최근에 본 상품</h6>
-      <div className="recProduct">
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="nextmenu">
-          <div className="num"></div>
-          <div className="num"></div>
-          <div className="num"></div>
-          <div className="num"></div>
-          <div className="num"></div>
-        </div>
-      </div>
-      <h6 className="nowProductTitle">현재 추천 상품</h6>
-      <div className="nowProduct">
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="pd">
-          <div className="productTitle">제티 2박스</div>
-          <div className="productPrice">₩ 9,920</div>
-          <div className="productN">1/N : ₩4,960</div>
-          <div className="member">1/2</div>
-        </div>
-        <div className="nextmenu">
-          <div className="num"></div>
-          <div className="num"></div>
-          <div className="num"></div>
-          <div className="num"></div>
-          <div className="num"></div>
-        </div>
-      </div>
+    <div className="MainWrap">
+      <div className="TopNav"></div>
     </div>
   );
 };
