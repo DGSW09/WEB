b356b59036fa7ab434079ec4804353e51cc5f136
jeonminchan <chan2bo2@dgsw.hs.kr>
2023-10-04 19:18:21 +0900

    FixedError: MainPage

--- project09/src/components/Main/Main.jsx
+++ project09/src/components/Main/Main.jsx
@@ -1,13 +1,103 @@
 import "../../styles/Main.css";
-import { FontAwsomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../img/logo.png";
 import profile from "../../img/profile.png";
-
+import TopNav from "../../img/TopNav.png";
+import searchIcon from "../../img/search.png";
 const Main = () => {
+  const ReloadPage = () => {
+    window.location.reload();
+  };
+
   return (
     <div className="MainWrap">
-      <div className="TopNav"></div>
+      <div className="TopNav">
+        <img src={logo} alt="09" id="Logo" onClick={ReloadPage} />
+        <input type="text" className="search" />
+        <img src={profile} className="profile" />
+        <FontAwesomeIcon icon={faPenToSquare} className="edit" />
+      </div>
+      <div className="recentProductTitle">최근에 본 상품</div>
+      <div className="recentProdcut">
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="nextmenu">
+          <div className="num"></div>
+          <div className="num"></div>
+          <div className="num"></div>
+          <div className="num"></div>
+          <div className="num"></div>
+        </div>
+      </div>
+      <h6 className="nowProductTitle">현재 추천 상품</h6>
+      <div className="nowProduct">
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="pd">
+          <div className="productTitle">제티 2박스</div>
+          <div className="productPrice">₩ 9,920</div>
+          <div className="productN">1/N : ₩4,960</div>
+        </div>
+        <div className="nextmenu">
+          <div className="num"></div>
+          <div className="num"></div>
+          <div className="num"></div>
+          <div className="num"></div>
+          <div className="num"></div>
+        </div>
+      </div>
     </div>
   );
 };
