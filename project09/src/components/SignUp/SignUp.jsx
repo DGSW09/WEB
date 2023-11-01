6463d53eb33c6ba1d6309d57ee286cca8a2f5a13
jeonminchan <chan2bo2@dgsw.hs.kr>
2023-11-01 09:16:10 +0900

    axios Error 해결

--- project09/src/components/SignUp/SignUp.jsx
+++ project09/src/components/SignUp/SignUp.jsx
@@ -4,23 +4,37 @@ import logo from "../../img/logo.png";
 import "../../styles/SignUp.css";
 import axios from "axios";
 const SignUp = () => {
+  const cliendId = "46330787138-22m388pdr4ssrehscaaksg1nbfr3qn03.apps.googleusercontent.com";
+  const SERVERURL = "http://172.20.10.13:8080/auth/login/google";
+
+  const onSuccess = async (response) => {
+    console.log(response);
+
+    try {
+      await axios.post(
+        SERVERURL,
+        {
+          idToken: `${response.credential}`,
+        },
+        {
+          headers: null,
+        },
+      );
+
+      console.log("Post Complete");
+    } catch (error) {
+      console.log("Post Failed", error);
+    }
+  };
   return (
-    <div className='signUpWrapper'>
-      <img src={logo} alt="09's logo" className='logo' />
-      <div>
-        <GoogleOAuthProvider clientId='46330787138-22m388pdr4ssrehscaaksg1nbfr3qn03.apps.googleusercontent.com'>
+    <div className="signUpWrapper">
+      <img src={logo} alt="09's logo" className="logo" />
+      <div className="GOOGLE">
+        <GoogleOAuthProvider clientId={cliendId}>
           <GoogleLogin
-            onSuccess={async (credentialResponse) => {
-              axios
-                .post("http://localhost:8080/auth/login/google", {
-                  id_token: `${credentialResponse.credential}`,
-                })
-                .then(({ data }) => {
-                  console.log(data);
-                });
-            }}
+            onSuccess={onSuccess}
             onError={() => {
-              console.log("Login Failed");
+              console.log("실패");
             }}
           />
         </GoogleOAuthProvider>
